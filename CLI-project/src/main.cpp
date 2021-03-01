a4dd1322ff51cb6e2a5c4119ca921cd7115f89aa
SpongeStark <yksponge@gmail.com>
2021-03-01 09:39:12 +0100

    test class : FloatPoint

--- CLI-project/src/main.cpp
+++ CLI-project/src/main.cpp
@@ -2,6 +2,8 @@
 #include "floatpoint.h"
 using namespace std;
 
+void testFloatPoint();
+
 int main(int argc, char* agrv[]){
   
   cout << "Hello World!" << endl;
@@ -11,7 +13,21 @@ int main(int argc, char* agrv[]){
 
 void testFloatPoint()
 {
+  cout << "Test the class : FloatPoint" << endl;
   FloatPoint point1(2.1, 3);
   FloatPoint point2(2, 5);
+  cout << "Test << and -" << endl;
   cout << point2 - point1 << endl;
+
+  FloatPoint point3(2,5);
+  cout << "Test == (true)" << endl;
+  if (point2 == point3)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  cout << "Test == (false)" << endl;
+  if (point1 == point3)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
 }
