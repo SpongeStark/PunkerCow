ea9d01c23f33ec739b3b4fd649db2fc67ad01308
DayreAming10YS <zyb397923792@gmail.com>
2021-05-17 10:18:21 +0200

    improve format

--- CLI-project/src/main.cpp
+++ CLI-project/src/main.cpp
@@ -18,19 +18,19 @@ int main()
   vector<FloatPoint> polygon;
 
   Flag:
-  cout << "Donnez le nombre de piquets ce que vous voulez bouger:"<<endl;
+  cout << "give the number of stakes which you want to move:" << endl;
   cin >> nb_piquets;
-  while(cin.fail())
+  while( cin.fail() )
   {
     cin.clear();
     cin.ignore();
-    cout <<"Erreur!Retapez le nombre SVP:" << endl;
+    cout << "error!you can only give the number,please retry:" << endl;
     cin >> nb_piquets;
   }
   
-  if(nb_piquets > 50 || nb_piquets <= 0)
+  if( nb_piquets > 50 || nb_piquets <= 0 )
   {
-      cout<<"le nombre est null"<<endl;
+      cout << "the number isn't in rage" << endl;
       goto Flag;
   }
 
@@ -38,10 +38,10 @@ int main()
 
   for( int i=0; i<nb_piquets; i++ )
   {
-    cout<<"saisir le piquet " << i << ":" << endl;
-    cout << "   le abscissa:" << endl;
+    cout << "give the position of the stake " << i << ":" << endl;
+    cout << "    the abscissa:" << endl;
     cin >> x;
-    cout << "   le ordinate:" << endl;
+    cout << "    the ordinate:" << endl;
     cin >> y;
     
     piquet = new FloatPoint(x,y);
@@ -51,55 +51,8 @@ int main()
 
 
 
-
-
-
-  testFloatPoint();
-  testVector();
-  testPolygon();
   return 0;
 }
 
-void testFloatPoint()
-{
-  cout  <<  "Test the class : FloatPoint"  <<  endl;
-  FloatPoint point1(2.1, 3);
-  FloatPoint point2(2, 5);
-  cout << "Test << and -" << endl;
-  cout << point2 - point1 << endl;
 
-  FloatPoint point3(2,5);
-  cout << "Test == (true)" << endl;
-  if (point2 == point3)
-    cout << "Yes" << endl;
-  else
-    cout << "No" << endl;
-  cout << "Test == (false)" << endl;
-  if (point1 == point3)
-    cout << "Yes" << endl;
-  else
-    cout << "No" << endl;
-}
 
-void testVector()
-{
-  cout << "Test the class : FloatVector" << endl;
-  FloatVector vector1(2.34, 4.56);
-  cout << "Test <<" << endl;
-  cout << vector1 << endl;
-  FloatPoint point1(2.1, 3);
-  FloatPoint point2(2, 5);
-  cout << "Test constructor" << endl;
-  FloatVector vector2(point1, point2);
-  cout << vector2 << endl;
-  cout << "Test + and -" << endl;
-  cout << "1+2 : " << vector1 + vector2 << endl;
-  cout << "1-2 : " << vector1 - vector2 << endl;
-  cout << "Test *" << endl;
-  cout << "1*2 : " << vector1 * vector2 << endl;
-}
-
-void testPolygon()
-{
-
-}
