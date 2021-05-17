01cac016df1a98cc1f7aa910b5cddd743444d9eb
DayreAming10YS <zyb397923792@gmail.com>
2021-05-17 09:12:54 +0200

    improve function main( )

--- CLI-project/src/main.cpp
+++ CLI-project/src/main.cpp
@@ -1,15 +1,58 @@
 #include <iostream>
 #include "floatpoint.h"
 #include "floatvector.h"
+#include<vector>
 
 using namespace std;
 
 void testFloatPoint();
 void testVector();
 
-int main(int argc, char* agrv[]){
+int main()
+{
   
-  cout << "Hello World!" << endl;
+  int nb_piquets;
+	double sum_x, sum_y, sum_area, area;
+  double x, y;
+  vector<FloatPoint> polygon;
+
+  Flag:
+  cout << "Donnez le nombre de piquets ce que vous voulez bouger:"<<endl;
+  cin >> nb_piquets;
+  while(cin.fail())
+  {
+    cin.clear();
+    cin.ignore();
+    cout <<"Erreur!Retapez le nombre SVP:" << endl;
+    cin >> nb_piquets;
+  }
+  
+  if(nb_piquets > 50 || nb_piquets <= 0)
+  {
+      cout<<"le nombre est null"<<endl;
+      goto Flag;
+  }
+
+  FloatPoint* piquet;  
+
+  for( int i=0; i<nb_piquets; i++ )
+  {
+    cout<<"saisir le piquet " << i << ":" << endl;
+    cout << "   le abscissa:" << endl;
+    cin >> x;
+    cout << "   le ordinate:" << endl;
+    cin >> y;
+    
+    piquet = new FloatPoint(x,y);
+    polygon.push_back(*piquet);
+  }
+	
+
+
+
+
+
+
   testFloatPoint();
   testVector();
   return 0;
@@ -17,7 +60,7 @@ int main(int argc, char* agrv[]){
 
 void testFloatPoint()
 {
-  cout << "Test the class : FloatPoint" << endl;
+  cout  <<  "Test the class : FloatPoint"  <<  endl;
   FloatPoint point1(2.1, 3);
   FloatPoint point2(2, 5);
   cout << "Test << and -" << endl;
